096ead1d5f2eb2af68bbcddf28d05d76f6b0f685
R::- <nicolasrfarias@gmail.com>
2016-08-22 21:52:02 -0500

    Header principal agregado

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,15 @@
-<html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
+<!DOCTYPE html>
+<html class="full">
+  <head>
+    <meta charset="utf-8">
+    <title>Projecto de euler</title>
 
-                <h1><?php echo $title; ?></h1>
+<!-- Link de bootstrap -->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Hoja para los estilos que estaran en todas las paginas del projecto -->
+<link rel="stylesheet" href=" <?php echo base_url(); ?>css/base.css" media="screen" title="no title" charset="utf-8">
+<!-- Hoja para los estilos de los distintos modulos -->
+<link rel="stylesheet" href=" <?php echo base_url(); ?>css/modulos.css" media="screen" title="no title" charset="utf-8">
+  </head>
